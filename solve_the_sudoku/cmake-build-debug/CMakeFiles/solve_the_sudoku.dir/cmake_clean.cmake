ac0b585c2e1a86201b3a1c5ddc746738051bce52
AllFromBCN <soominjeong93@gmail.com>
2018-03-12 23:24:42 +0900

    Initial upload for solve the sudoku

---
+++ solve_the_sudoku/cmake-build-debug/CMakeFiles/solve_the_sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solve_the_sudoku.dir/main.cpp.o"
+  "solve_the_sudoku.pdb"
+  "solve_the_sudoku"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solve_the_sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
